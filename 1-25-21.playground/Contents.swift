1b907daf48d6b1e00c19f4181b9efe15c2ef55cc
Armanul Ambia <arman@Arman-M1.local>
2022-01-27 14:14:52 -0800

    first week of class

---
+++ 1-25-21.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Code snippet 3
+
+var name = "Tuffy Titan"
+var city = "Fullerton"
+var state = "CA"
+
+/* The \( ) symbol is called a String interpolation. It embeds the
+   values of variables and expressions in the specified position of a
+   String.
+
+   print is a function that displays Strings on the console.
+*/
+print("Hello I am \(name) from \(city), \(state)");
